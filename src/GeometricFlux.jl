83f4550a34aefa7f82fdcaf561f0bea7dac03a95
Yueh-Hua Tu <a504082002@gmail.com>
2019-06-30 13:22:00 +0800

    Add depedencies

--- src/GeometricFlux.jl
+++ src/GeometricFlux.jl
@@ -26,14 +26,9 @@ include("layers.jl")
 include("linalg.jl")
 
 function __init__()
-    @require MetaGraphs = "626554b9-1ddb-594c-aa3c-2596fe9399a5" begin
-        include("metagraphs.jl")
-        @require CuArrays = "3a865a2d-5b23-5a0f-bc46-62713ec82fae" include("cuda/metagraphs.jl")
-    end
-
-    @require CuArrays = "3a865a2d-5b23-5a0f-bc46-62713ec82fae" begin
-        @require MetaGraphs = "626554b9-1ddb-594c-aa3c-2596fe9399a5" include("cuda/metagraphs.jl")
-    end
+    @require LightGraphs = "093fc24a-ae57-5d10-9952-331d41423f4d" include("graph/simplegraphs.jl")
+    @require SimpleWeightedGraphs = "47aef6b3-ad0c-573a-a1e2-d07658019622" include("graph/weightedgraphs.jl")
+    @require MetaGraphs = "626554b9-1ddb-594c-aa3c-2596fe9399a5" include("metagraphs.jl")
 end
 
 end
---
+++ src/graph/simplegraphs.jl
@@ -0,0 +1,22 @@
+function GCNConv(g::AbstractSimpleGraph, ch::Pair{<:Integer,<:Integer}, σ = identity;
+                 init = glorot_uniform)
+    GCNConv(param(init(ch[2], ch[1])), param(laplacian_matrix(adj+I)), σ)
+end
+
+
+function ChebConv(g::AbstractSimpleGraph, ch::Pair{<:Integer,<:Integer}, k::Integer;
+                  init = glorot_uniform)
+    return ChebConv(, zeros(size(X, 1), size(X, 2), k))
+end
+
+
+function GraphConv(g::AbstractSimpleGraph, ch::Pair{<:Integer,<:Integer}, aggr::Symbol;
+                   init = glorot_uniform)
+    return GraphConv(, init(ch[2], ch[1]), aggr)
+end
+
+
+function GATConv(g::AbstractSimpleGraph, ch::Pair{<:Integer,<:Integer};
+                 heads=1, concat=True, negative_slope=0.2, dropout=0)
+    return GATConv()
+end
