e6c00568a6aa92dfc4466dfb8656972e5bc53346
Yueh-Hua Tu <a504082002@gmail.com>
2019-08-12 23:37:31 +0800

    GCNConv layer works with CUDA

--- examples/gcn.jl
+++ examples/gcn.jl
@@ -5,6 +5,7 @@ using JLD2
 using StatsBase
 using SparseArrays
 using LightGraphs.SimpleGraphs
+using CuArrays
 
 @load "data/cora_features.jld2" features
 @load "data/cora_labels.jld2" labels
@@ -13,16 +14,17 @@ using LightGraphs.SimpleGraphs
 num_nodes = 2708
 num_features = 1433
 target_catg = 7
+epochs = 10
 
 ## Preprocessing data
-train_X = features  # dim: num_features * num_nodes
-train_y = labels  # dim: target_catg * num_nodes
+train_X = features |> gpu  # dim: num_features * num_nodes
+train_y = labels |> gpu  # dim: target_catg * num_nodes
 
 ## Model
 model = Chain(GCNConv(g, num_features=>1000, relu),
               GCNConv(g, 1000=>500, relu),
-              GCNConv(g, 500=>7),
-              softmax)
+              Dense(500, 7),
+              softmax) |> gpu
 # test model
 # model(train_X)
 
@@ -36,4 +38,6 @@ train_data = [(train_X, train_y)]
 opt = ADAM(0.01)
 evalcb() = @show(accuracy(train_X, train_y))
 
-Flux.train!(loss, ps, train_data, opt, cb=throttle(evalcb, 10))
+for i = 1:epochs
+    Flux.train!(loss, ps, train_data, opt, cb=throttle(evalcb, 10))
+end
