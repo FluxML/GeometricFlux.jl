21144a753aada03520950f83557099958bf0b612
Yueh-Hua Tu <a504082002@gmail.com>
2019-09-11 13:11:14 +0800

    Refactor tests

--- test/graph/weightedgraphs.jl
+++ test/graph/weightedgraphs.jl
@@ -15,48 +15,50 @@ ug = SimpleWeightedGraph(6)
 add_edge!(ug, 1, 2, 2); add_edge!(ug, 1, 3, 2); add_edge!(ug, 2, 3, 1)
 add_edge!(ug, 3, 4, 5); add_edge!(ug, 2, 5, 2); add_edge!(ug, 3, 6, 2)
 
-@testset "Test support of SimpleWeightedGraphs for GCNConv layer" begin
-    gc = GCNConv(ug, in_channel=>out_channel)
-    @test size(gc.weight) == (out_channel, in_channel)
-    @test size(gc.bias) == (out_channel, N)
-    @test size(gc.norm) == (N, N)
-end
 
+@testset "weightedgraphs" begin
+    @testset "GCNConv" begin
+        gc = GCNConv(ug, in_channel=>out_channel)
+        @test size(gc.weight) == (out_channel, in_channel)
+        @test size(gc.bias) == (out_channel, N)
+        @test size(gc.norm) == (N, N)
+    end
 
-@testset "Test support of SimpleWeightedGraphs for ChebConv layer" begin
-    k = 4
-    cc = ChebConv(ug, in_channel=>out_channel, k)
-    @test size(cc.weight) == (out_channel, in_channel, k)
-    @test size(cc.bias) == (out_channel, N)
-    @test size(cc.L̃) == (N, N)
-    @test cc.k == k
-    @test cc.in_channel == in_channel
-    @test cc.out_channel == out_channel
-end
+    @testset "ChebConv" begin
+        k = 4
+        cc = ChebConv(ug, in_channel=>out_channel, k)
+        @test size(cc.weight) == (out_channel, in_channel, k)
+        @test size(cc.bias) == (out_channel, N)
+        @test size(cc.L̃) == (N, N)
+        @test cc.k == k
+        @test cc.in_channel == in_channel
+        @test cc.out_channel == out_channel
+    end
 
-@testset "Test support of SimpleWeightedGraphs for GraphConv layer" begin
-    gc = GraphConv(ug, in_channel=>out_channel)
-    @test gc.adjlist == [[2,3], [1,3,5], [1,2,4,6], [3], [2], [3]]
-    @test size(gc.weight1) == (out_channel, in_channel)
-    @test size(gc.weight2) == (out_channel, in_channel)
-    @test size(gc.bias) == (out_channel, N)
-end
+    @testset "GraphConv" begin
+        gc = GraphConv(ug, in_channel=>out_channel)
+        @test gc.adjlist == [[2,3], [1,3,5], [1,2,4,6], [3], [2], [3]]
+        @test size(gc.weight1) == (out_channel, in_channel)
+        @test size(gc.weight2) == (out_channel, in_channel)
+        @test size(gc.bias) == (out_channel, N)
+    end
 
-@testset "Test support of SimpleWeightedGraphs for GATConv layer" begin
-    gat = GATConv(ug, in_channel=>out_channel)
-    @test gat.adjlist == [[2,3], [1,3,5], [1,2,4,6], [3], [2], [3]]
-    @test size(gat.weight) == (out_channel, in_channel)
-    @test size(gat.bias) == (out_channel, N)
-end
+    @testset "GATConv" begin
+        gat = GATConv(ug, in_channel=>out_channel)
+        @test gat.adjlist == [[2,3], [1,3,5], [1,2,4,6], [3], [2], [3]]
+        @test size(gat.weight) == (out_channel, in_channel)
+        @test size(gat.bias) == (out_channel, N)
+    end
 
-@testset "Test support of SimpleWeightedGraphs for GatedGraphConv layer" begin
-    num_layers = 3
-    ggc = GatedGraphConv(ug, out_channel, num_layers)
-    @test ggc.adjlist == [[2,3], [1,3,5], [1,2,4,6], [3], [2], [3]]
-    @test size(ggc.weight) == (out_channel, out_channel, num_layers)
-end
+    @testset "GatedGraphConv" begin
+        num_layers = 3
+        ggc = GatedGraphConv(ug, out_channel, num_layers)
+        @test ggc.adjlist == [[2,3], [1,3,5], [1,2,4,6], [3], [2], [3]]
+        @test size(ggc.weight) == (out_channel, out_channel, num_layers)
+    end
 
-@testset "Test support of SimpleWeightedGraphs for EdgeConv layer" begin
-    ec = EdgeConv(ug, Dense(2*in_channel, out_channel))
-    @test ec.adjlist == [[2,3], [1,3,5], [1,2,4,6], [3], [2], [3]]
+    @testset "EdgeConv" begin
+        ec = EdgeConv(ug, Dense(2*in_channel, out_channel))
+        @test ec.adjlist == [[2,3], [1,3,5], [1,2,4,6], [3], [2], [3]]
+    end
 end
--- test/layers/conv.jl
+++ test/layers/conv.jl
@@ -9,72 +9,73 @@ adj = [0. 1. 0. 1.;
        1. 0. 1. 0.]
 
 
-@testset "Test GCNConv layer" begin
-    gc = GCNConv(adj, in_channel=>out_channel)
-    @test size(gc.weight) == (out_channel, in_channel)
-    @test size(gc.bias) == (out_channel, N)
-    @test size(gc.norm) == (N, N)
+@testset "layer" begin
+    @testset "GCNConv" begin
+        gc = GCNConv(adj, in_channel=>out_channel)
+        @test size(gc.weight) == (out_channel, in_channel)
+        @test size(gc.bias) == (out_channel, N)
+        @test size(gc.norm) == (N, N)
 
-    X = rand(in_channel, N)
-    Y = gc(X)
-    @test size(Y) == (out_channel, N)
-end
+        X = rand(in_channel, N)
+        Y = gc(X)
+        @test size(Y) == (out_channel, N)
+    end
 
+    @testset "ChebConv" begin
+        k = 6
+        cc = ChebConv(adj, in_channel=>out_channel, k)
+        @test size(cc.weight) == (out_channel, in_channel, k)
+        @test size(cc.bias) == (out_channel, N)
+        @test size(cc.L̃) == (N, N)
+        @test cc.k == k
+        @test cc.in_channel == in_channel
+        @test cc.out_channel == out_channel
 
-@testset "Test ChebConv layer" begin
-    k = 6
-    cc = ChebConv(adj, in_channel=>out_channel, k)
-    @test size(cc.weight) == (out_channel, in_channel, k)
-    @test size(cc.bias) == (out_channel, N)
-    @test size(cc.L̃) == (N, N)
-    @test cc.k == k
-    @test cc.in_channel == in_channel
-    @test cc.out_channel == out_channel
+        X = rand(in_channel, N)
+        Y = cc(X)
+        @test size(Y) == (out_channel, N)
+    end
 
-    X = rand(in_channel, N)
-    Y = cc(X)
-    @test size(Y) == (out_channel, N)
-end
+    @testset "GraphConv" begin
+        gc = GraphConv(adj, in_channel=>out_channel)
+        @test gc.adjlist == [[2,4], [1,3], [2,4], [1,3]]
+        @test size(gc.weight1) == (out_channel, in_channel)
+        @test size(gc.weight2) == (out_channel, in_channel)
+        @test size(gc.bias) == (out_channel, N)
 
-@testset "Test GraphConv layer" begin
-    gc = GraphConv(adj, in_channel=>out_channel)
-    @test gc.adjlist == [[2,4], [1,3], [2,4], [1,3]]
-    @test size(gc.weight1) == (out_channel, in_channel)
-    @test size(gc.weight2) == (out_channel, in_channel)
-    @test size(gc.bias) == (out_channel, N)
+        X = rand(in_channel, N)
+        Y = gc(X)
+        @test size(Y) == (out_channel, N)
+    end
 
-    X = rand(in_channel, N)
-    Y = gc(X)
-    @test size(Y) == (out_channel, N)
-end
+    @testset "GATConv" begin
+        gat = GATConv(adj, in_channel=>out_channel)
+        @test gat.adjlist == [[2,4], [1,3], [2,4], [1,3]]
+        @test size(gat.weight) == (out_channel, in_channel)
+        @test size(gat.bias) == (out_channel, N)
 
-@testset "Test GATConv layer" begin
-    gat = GATConv(adj, in_channel=>out_channel)
-    @test gat.adjlist == [[2,4], [1,3], [2,4], [1,3]]
-    @test size(gat.weight) == (out_channel, in_channel)
-    @test size(gat.bias) == (out_channel, N)
+        X = rand(in_channel, N)
+        Y = gat(X)
+        @test size(Y) == (out_channel, N)
+    end
 
-    X = rand(in_channel, N)
-    Y = gat(X)
-    @test size(Y) == (out_channel, N)
-end
+    @testset "GatedGraphConv" begin
+        num_layers = 3
+        ggc = GatedGraphConv(adj, out_channel, num_layers)
+        @test ggc.adjlist == [[2,4], [1,3], [2,4], [1,3]]
+        @test size(ggc.weight) == (out_channel, out_channel, num_layers)
 
-@testset "Test GatedGraphConv layer" begin
-    num_layers = 3
-    ggc = GatedGraphConv(adj, out_channel, num_layers)
-    @test ggc.adjlist == [[2,4], [1,3], [2,4], [1,3]]
-    @test size(ggc.weight) == (out_channel, out_channel, num_layers)
+        X = rand(in_channel, N)
+        Y = ggc(X)
+        @test size(Y) == (out_channel, N)
+    end
 
-    X = rand(in_channel, N)
-    Y = ggc(X)
-    @test size(Y) == (out_channel, N)
-end
+    @testset "EdgeConv" begin
+        ec = EdgeConv(adj, Dense(2*in_channel, out_channel))
+        @test ec.adjlist == [[2,4], [1,3], [2,4], [1,3]]
 
-@testset "Test EdgeConv layer" begin
-    ec = EdgeConv(adj, Dense(2*in_channel, out_channel))
-    @test ec.adjlist == [[2,4], [1,3], [2,4], [1,3]]
-
-    X = rand(in_channel, N)
-    Y = ec(X)
-    @test size(Y) == (out_channel, N)
+        X = rand(in_channel, N)
+        Y = ec(X)
+        @test size(Y) == (out_channel, N)
+    end
 end
