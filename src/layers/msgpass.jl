67dfd21b9edc8cb92f0b3f23fdac538e668bf9ed
Yueh-Hua Tu <a504082002@gmail.com>
2020-07-06 10:39:18 +0800

    Message passing test passed
    Correct message passing update bug

--- src/layers/msgpass.jl
+++ src/layers/msgpass.jl
@@ -1,60 +1,73 @@
 abstract type MessagePassing <: GraphNet end
 
 """
-    message(m, x_i, x_j, e_ij)
+    message(mp, x_i, x_j, e_ij)
 
 Message function for message-passing scheme. This function can be overrided to dispatch to custom layers.
 First argument should be message-passing layer, the rest of arguments can be `x_i`, `x_j` and `e_ij`.
 
 # Arguments
-- `m`: message-passing layer.
+- `mp`: message-passing layer.
 - `x_i`: the feature of node `x_i`.
 - `x_j`: the feature of neighbors of node `x_i`.
 - `e_ij`: the feature of edge (`x_i`, `x_j`).
 """
-@inline message(m::T, x_i, x_j, e_ij) where {T<:MessagePassing} = x_j
+@inline message(mp::T, x_i, x_j, e_ij) where {T<:MessagePassing} = x_j
 
 """
-    update(m, X, M)
+    update(mp, m, x)
 
 Update function for message-passing scheme. This function can be overrided to dispatch to custom layers.
 First argument should be message-passing layer, the rest of arguments can be `X` and `M`.
 
 # Arguments
-- `m`: message-passing layer.
+- `mp`: message-passing layer.
+- `m`: the message aggregated from message function.
 - `x`: the single node feature.
-- `msg`: the message aggregated from message function.
 """
-@inline update(m::T, x, msg) where {T<:MessagePassing} = msg
+@inline update(mp::T, m, x) where {T<:MessagePassing} = m
 
-@inline function update_batch_edge(m::T, E::AbstractMatrix, X::AbstractMatrix, adj) where {T<:MessagePassing}
+@inline function update_batch_edge(mp::T, adj, E::AbstractMatrix, X::AbstractMatrix) where {T<:MessagePassing}
     edge_idx = edge_index_table(adj)
-    M = Vector[]
+    E_ = Vector[]
     for (i, js) = enumerate(adj)
         for j = js
             k = edge_idx[(i,j)]
-            m = message(m, get_feature(X, i), get_feature(X, j), get_feature(E, k))
-            push!(M, m)
+            m = message(mp, get_feature(X, i), get_feature(X, j), get_feature(E, k))
+            push!(E_, m)
         end
     end
-    hcat(M...)
+    hcat(E_...)
 end
 
-@inline function update_batch_vertex(m::T, M::AbstractMatrix, X::AbstractMatrix) where {T<:MessagePassing}
+@inline function update_batch_vertex(mp::T, M::AbstractMatrix, X::AbstractMatrix) where {T<:MessagePassing}
     X_ = Vector[]
     for i = 1:size(X,2)
-        x = update(m, get_feature(M, i), get_feature(X, i))
+        x = update(mp, get_feature(M, i), get_feature(X, i))
         push!(X_, x)
     end
     hcat(X_...)
 end
 
-@inline function aggregate_neighbors(m::T, aggr::Symbol, M::AbstractMatrix, accu_edge, num_V, num_E) where {T<:MessagePassing}
+@inline function aggregate_neighbors(mp::T, aggr::Symbol, M::AbstractMatrix, accu_edge, num_V, num_E) where {T<:MessagePassing}
     @assert !iszero(accu_edge) "accumulated edge must not be zero."
     cluster = generate_cluster(M, accu_edge, num_V, num_E)
     pool(aggr, cluster, M)
 end
 
 function propagate(mp::T, fg::FeaturedGraph, aggr::Symbol=:add) where {T<:MessagePassing}
-    _propagate(mp, fg, aggr, nothing, nothing)
+    adj = neighbors(fg)
+    num_V = nv(fg)
+    accu_edge = accumulated_edges(adj)
+    num_E = accu_edge[end]
+    E = edge_feature(fg)
+    X = node_feature(fg)
+
+    E = update_batch_edge(mp, adj, E, X)
+
+    M = aggregate_neighbors(mp, aggr, E, accu_edge, num_V, num_E)
+
+    X = update_batch_vertex(mp, M, X)
+
+    FeaturedGraph(graph(fg), X, E, zeros(0))
 end
--- test/layers/msgpass.jl
+++ test/layers/msgpass.jl
@@ -1,5 +1,3 @@
-import GeometricFlux: message
-
 in_channel = 10
 out_channel = 5
 num_V = 6
@@ -17,18 +15,40 @@ struct NewLayer <: MessagePassing
 end
 NewLayer(m, n) = NewLayer(randn(m,n))
 
-message(l::NewLayer, x_i, x_j, e_ij) = l.weight * x_j
 (l::NewLayer)(fg) = propagate(l, fg, :add)
 
 X = Array(reshape(1:num_V*in_channel, in_channel, num_V))
+fg = FeaturedGraph(adj, X)
+
+l = NewLayer(out_channel, in_channel)
 
 @testset "msgpass" begin
-    fg = FeaturedGraph(adj, X)
-    l = NewLayer(out_channel, in_channel)
-    fg_ = l(fg)
+    @testset "no message or update" begin
+        fg_ = l(fg)
 
-    @test graph(fg_) === adj
-    @test size(node_feature(fg_)) == (out_channel, num_V)
-    @test size(edge_feature(fg_)) == (0, 2*num_E)
-    @test size(global_feature(fg_)) == (0,)
+        @test graph(fg_) === adj
+        @test size(node_feature(fg_)) == (in_channel, num_V)
+        @test size(edge_feature(fg_)) == (in_channel, 2*num_E)
+        @test size(global_feature(fg_)) == (0,)
+    end
+
+    GeometricFlux.message(l::NewLayer, x_i, x_j, e_ij) = l.weight * x_j
+    @testset "message function" begin
+        fg_ = l(fg)
+
+        @test graph(fg_) === adj
+        @test size(node_feature(fg_)) == (out_channel, num_V)
+        @test size(edge_feature(fg_)) == (out_channel, 2*num_E)
+        @test size(global_feature(fg_)) == (0,)
+    end
+
+    GeometricFlux.update(l::NewLayer, m, x) = l.weight * x + m
+    @testset "message and update" begin
+        fg_ = l(fg)
+
+        @test graph(fg_) === adj
+        @test size(node_feature(fg_)) == (out_channel, num_V)
+        @test size(edge_feature(fg_)) == (out_channel, 2*num_E)
+        @test size(global_feature(fg_)) == (0,)
+    end
 end
