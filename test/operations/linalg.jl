90d015e95dd984d8b060c000997b4725250ded22
Yueh-Hua Tu <a504082002@gmail.com>
2020-07-08 10:23:26 +0800

    Add more test for linear algebra

--- test/operations/linalg.jl
+++ test/operations/linalg.jl
@@ -16,21 +16,27 @@
                    -.5 1. -.5 0.;
                    0. -.5 1. -.5;
                    -.5 0. -.5 1.]
-        scaled_lap =   [0 -0.5 0 -0.5; 
-                        -0.5 0 -0.5 -0; 
-                        0 -0.5 0 -0.5; 
+        scaled_lap =   [0 -0.5 0 -0.5;
+                        -0.5 0 -0.5 -0;
+                        0 -0.5 0 -0.5;
                         -0.5 0 -0.5 0]
 
-        for T in [Int8, Float64]
+        for T in [Int8, Int16, Int32, Int64, Float16, Float32, Float64]
             @test degree_matrix(adj, T, dir=:out) == T.(deg)
             @test degree_matrix(adj, T, dir=:out) == degree_matrix(adj, T, dir=:in)
             @test degree_matrix(adj, T, dir=:out) == degree_matrix(adj, T, dir=:both)
+            @test eltype(degree_matrix(adj, T, dir=:out)) == T
+
             @test laplacian_matrix(adj, T) == T.(lap)
+            @test eltype(laplacian_matrix(adj, T)) == T
         end
-        @test normalized_laplacian(adj, Float64) ≈ norm_lap
-        @test eltype(normalized_laplacian(adj, Float32)) == Float32
-        @test scaled_laplacian(adj, Float64) ≈ scaled_lap
-        @test eltype(scaled_laplacian(adj, Float32)) == Float32
+        for T in [Float16, Float32, Float64]
+            @test normalized_laplacian(adj, T) ≈ T.(norm_lap)
+            @test eltype(normalized_laplacian(adj, T)) == T
+            
+            @test scaled_laplacian(adj, T) ≈ T.(scaled_lap)
+            @test eltype(scaled_laplacian(adj, T)) == T
+        end
         @test neighbors(adj) == [[2,4], [1,3], [2,4], [1,3]]
     end
 
@@ -52,14 +58,21 @@
                     0 0 7 0;
                     0 0 0 4]
 
-        for T in [Int8, Float64]
+        for T in [Int8, Int16, Int32, Int64, Float16, Float32, Float64]
             @test degree_matrix(adj, T, dir=:out) == T.(deg_out)
             @test degree_matrix(adj, T, dir=:in) == T.(deg_in)
             @test degree_matrix(adj, T, dir=:both) == T.(deg_both)
+            @test eltype(degree_matrix(adj, T, dir=:out)) == T
+            @test eltype(degree_matrix(adj, T, dir=:in)) == T
+            @test eltype(degree_matrix(adj, T, dir=:both)) == T
             @test_throws DomainError degree_matrix(adj, dir=:other)
+
             @test laplacian_matrix(adj, T, dir=:out) == T.(deg_out .- adj)
             @test laplacian_matrix(adj, T, dir=:in) == T.(deg_in .- adj)
             @test laplacian_matrix(adj, T, dir=:both) == T.(deg_both .- adj)
+            @test eltype(laplacian_matrix(adj, T, dir=:out)) == T
+            @test eltype(laplacian_matrix(adj, T, dir=:in)) == T
+            @test eltype(laplacian_matrix(adj, T, dir=:both)) == T
         end
         @test neighbors(adj) == [[2,3,4], [1,3], [1,2,4], [1,3]]
     end
