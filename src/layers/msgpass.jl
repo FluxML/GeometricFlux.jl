23f7eac573a5fb24fb0a871172ca6b064cfaba7a
Yueh-Hua Tu <a504082002@gmail.com>
2020-07-11 10:00:02 +0800

    Make MessagePassing differentiable

--- src/layers/msgpass.jl
+++ src/layers/msgpass.jl
@@ -29,23 +29,15 @@ First argument should be message-passing layer, the rest of arguments can be `X`
 
 @inline function update_batch_edge(mp::T, adj, E::AbstractMatrix, X::AbstractMatrix) where {T<:MessagePassing}
     edge_idx = edge_index_table(adj)
-    E_ = Vector[]
-    for (i, js) = enumerate(adj)
-        for j = js
-            k = edge_idx[(i,j)]
-            m = message(mp, get_feature(X, i), get_feature(X, j), get_feature(E, k))
-            push!(E_, m)
-        end
-    end
+    edges = map(x -> map((i,j) -> (i,j), repeat([x[1]],length(x[2])), x[2]), enumerate(adj))
+    ij = vcat(edges...)
+    ijk = map(x -> (x[1], x[2], edge_idx[(x[1],x[2])]), ij)
+    E_ = map(x -> message(mp, get_feature(X, x[1]), get_feature(X, x[2]), get_feature(E, x[3])), ijk)
     hcat(E_...)
 end
 
 @inline function update_batch_vertex(mp::T, M::AbstractMatrix, X::AbstractMatrix) where {T<:MessagePassing}
-    X_ = Vector[]
-    for i = 1:size(X,2)
-        x = update(mp, get_feature(M, i), get_feature(X, i))
-        push!(X_, x)
-    end
+    X_ = map(i -> update(mp, get_feature(M, i), get_feature(X, i)), 1:size(X,2))
     hcat(X_...)
 end
 
