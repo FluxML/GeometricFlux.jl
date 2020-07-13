264b11a91d233cb9c6860d71c3c25e4b85636867
Yueh-Hua Tu <a504082002@gmail.com>
2020-07-13 09:28:51 +0800

    Fix GAT example

--- examples/gat.jl
+++ examples/gat.jl
@@ -6,6 +6,7 @@ using JLD2  # use v0.1.2
 using Statistics: mean
 using SparseArrays
 using LightGraphs.SimpleGraphs
+using LightGraphs: adjacency_matrix
 using CuArrays
 
 @load "data/cora_features.jld2" features
@@ -14,20 +15,20 @@ using CuArrays
 
 num_nodes = 2708
 num_features = 1433
-
 heads  = 8
 hidden = 8
 target_catg = 7
 epochs = 10
 
 ## Preprocessing data
-train_X = features |> gpu  # dim: num_features * num_nodes
-train_y = labels |> gpu  # dim: target_catg * num_nodes
+train_X = Float32.(features) |> gpu  # dim: num_features * num_nodes
+train_y = Float32.(labels) |> gpu  # dim: target_catg * num_nodes
+adj_mat = Matrix{Float32}(adjacency_matrix(g)) |> gpu
 
 ## Model
 model = Chain(GATConv(g, num_features=>hidden, heads=heads),
               Dropout(0.6),
-              GATConv(g, hidden=>target_catg, heads=heads),
+              GATConv(g, hidden*heads=>target_catg, heads=heads, concat=false),
               softmax) |> gpu
 # test model
 # model(train_X)
@@ -35,6 +36,9 @@ model = Chain(GATConv(g, num_features=>hidden, heads=heads),
 ## Loss
 loss(x, y) = logitcrossentropy(model(x), y)
 accuracy(x, y) = mean(onecold(model(x)) .== onecold(y))
+
+# test gradient
+# gradient(X -> loss(X, train_y), train_X)
 
 ## Training
 ps = Flux.params(model)
