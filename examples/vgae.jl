7e29b92a2226519e1c80f2975b5ea4dc6a4d1fb8
Yueh-Hua Tu <a504082002@gmail.com>
2020-08-27 14:09:27 +0800

    VGAE example can be trained but loss goes to NaN

--- examples/vgae.jl
+++ examples/vgae.jl
@@ -1,62 +1,55 @@
 using GeometricFlux
 using Flux
 using Flux: throttle
+using Flux.Losses: logitbinarycrossentropy
 using Flux: @epochs
 using JLD2  # use v0.1.2
 using Distributions
 using SparseArrays
 using LightGraphs.SimpleGraphs
 using LightGraphs: adjacency_matrix
-using CUDA
-
-import Distributions: logpdf
 
 @load "data/cora_features.jld2" features
 @load "data/cora_graph.jld2" g
 
 num_nodes = 2708
 num_features = 1433
-h_dim = 32
+hidden1 = 128
+hidden2 = 32
 z_dim = 16
 target_catg = 7
 epochs = 200
 
 ## Preprocessing data
-adj_mat = Matrix{Float32}(adjacency_matrix(g))# |> gpu
-train_X = [Matrix{Float32}(features) for i = 1:10]# |> gpu  # dim: num_features * num_nodes
-train_y = [adj_mat for i = 1:10]  # dim: num_nodes * num_nodes
+adj_mat = Matrix{Float32}(adjacency_matrix(g))
+train_data = [(Matrix{Float32}(features), adj_mat)]
 
 ## Model
-model = VGAE(GCNConv(adj_mat, num_features=>h_dim, relu),
-             h_dim, z_dim)# |> gpu
+encoder = Chain(GCNConv(adj_mat, num_features=>hidden1, relu),
+                GCNConv(adj_mat, hidden1=>hidden2))
+model = VGAE(encoder, hidden2, z_dim)
 encoder = model.encoder
 decoder = model.decoder
+ps = Flux.params(model)
 
-logpdf(b::Bernoulli, y::Bool, T::Type{Real}=Float32) =
-    y * log(b.p + eps(T)) + (one(T) - y) * log(one(T) - b.p + eps(T))
+l2_norm(p) = sum(abs2, p)
 
-# KL-divergence between approximation posterior and N(0, 1) prior.
-kl_q_p(μ, logσ, T::Type{Real}=Float32) =
-    T(0.5) * sum(exp.(T(2) .* logσ) + μ.^2 .- one(T) .+ logσ.^2)
-
-# logp(x|z) - decoder
-logp_x_z(x, z) = sum(logpdf.(Bernoulli.(decoder(z)), x))
-
-function loss(X, Y, T=eltype(X), β=one(T), λ=T(0.01))
-    N = length(X)
-    f = (x) -> summarize(encoder, encoder.nn(x))
-    stats = f.(X)
-    μ̂ = [s[1] for s in stats]
-    logσ̂ = [s[2] for s in stats]
-    Z = encoder.(X)
-    L̄ = 1//N * (logp_x_z(X, Z) - β * kl_q_p(μ̂, logσ̂))
-    -L̄ + λ * sum(abs2, Flux.params(model))
+function loss(X, Y, T=eltype(X), β=one(T), λ=T(0.01); debug=false)
+    μ̂, logσ̂ = summarize(encoder, X)
+    Z = encoder(X)
+    kl_q_p = -T(0.5) * sum(one(T) .+ T(2).*logσ̂ .- μ̂.^2 .- exp.(T(2).*logσ̂))
+    logp_y_z = -sum(logitbinarycrossentropy(decoder(Z), Y, agg=identity)) / size(Y,2)
+    l2reg = sum(l2_norm, ps)
+    debug && begin
+        @show kl_q_p
+        @show logp_y_z
+        @show l2reg
+    end
+    -logp_y_z + β*kl_q_p + λ*l2reg
 end
 
 ## Training
-ps = Flux.params(model)
-train_data = [(train_X, train_y)]
-opt = ADAM(0.01)
-evalcb() = @show(loss(train_X, train_y))
+opt = ADAM(0.001)
+evalcb() = @show(loss(train_data[1]...))
 
 @epochs epochs Flux.train!(loss, ps, train_data, opt, cb=throttle(evalcb, 10))
