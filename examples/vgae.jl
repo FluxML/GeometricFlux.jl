f5b4c8d31640ac03b33ad78c6150657dc7b3b712
Yueh-Hua Tu <a504082002@gmail.com>
2020-08-29 00:36:00 +0800

    Correct VGAE example

--- examples/vgae.jl
+++ examples/vgae.jl
@@ -15,8 +15,7 @@ using LightGraphs: adjacency_matrix
 
 num_nodes = 2708
 num_features = 1433
-hidden1 = 128
-hidden2 = 32
+h_dim = 32
 z_dim = 16
 target_catg = 7
 epochs = 200
@@ -27,9 +26,7 @@ adj_mat = Matrix{Float32}(adjacency_matrix(g))
 train_data = [(FeaturedGraph(adj_mat.*M, Matrix{Float32}(features)), adj_mat) for M in masks]
 
 ## Model
-encoder = Chain(GCNConv(num_features=>hidden1, relu; cache=false),
-                GCNConv(hidden1=>hidden2; cache=false))
-model = VGAE(encoder, hidden2, z_dim, σ)
+model = VGAE(GCNConv(num_features=>h_dim, relu; cache=false), h_dim, z_dim, σ)
 encoder = model.encoder
 decoder = model.decoder
 ps = Flux.params(model)
--- test/models.jl
+++ test/models.jl
@@ -20,28 +20,42 @@ adj = [0. 1. 0. 1.;
     @testset "VGAE" begin
         @testset "InnerProductDecoder" begin
            ipd = InnerProductDecoder(identity)
-           Y = ipd(rand(1, N))
+           X = rand(1, N)
+           Y = ipd(X)
            @test size(Y) == (N, N)
 
-           Y = ipd(rand(in_channel, N))
+           X = rand(1, N)
+           fg = FeaturedGraph(adj, X)
+           fg_ = ipd(fg)
+           Y = node_feature(fg_)
+           @test size(Y) == (N, N)
+
+           X = rand(in_channel, N)
+           fg = FeaturedGraph(adj, X)
+           fg_ = ipd(fg)
+           Y = node_feature(fg_)
            @test size(Y) == (N, N)
         end
 
         @testset "VariationalEncoder" begin
             z_dim = 2
-            gc = GCNConv(adj, in_channel=>out_channel)
+            gc = GCNConv(in_channel=>out_channel)
             ve = VariationalEncoder(gc, out_channel, z_dim)
             X = rand(in_channel, N)
-            Z = ve(X)
+            fg = FeaturedGraph(adj, X)
+            fg_ = ve(fg)
+            Z = node_feature(fg_)
             @test size(Z) == (z_dim, N)
         end
 
         @testset "VGAE" begin
             z_dim = 2
-            gc = GCNConv(adj, in_channel=>out_channel)
+            gc = GCNConv(in_channel=>out_channel)
             vgae = VGAE(gc, out_channel, z_dim)
             X = rand(in_channel, N)
-            Y = vgae(X)
+            fg = FeaturedGraph(adj, X)
+            fg_ = vgae(fg)
+            Y = node_feature(fg_)
             @test size(Y) == (N, N)
         end
     end
