c2fe6b2e6fb7546af8d1f134ae6c972c97d49c33
Yueh-Hua Tu <a504082002@gmail.com>
2020-10-09 14:42:40 +0800

    Remove assign!

--- src/utils.jl
+++ src/utils.jl
@@ -1,30 +1,3 @@
-## Indexing
-
-function range_indecies(idx::Tuple)
-    x = Vector{Any}(undef, length(idx))
-    for (i,n) in enumerate(idx)
-        x[i] = 1:n
-    end
-    x
-end
-
-replace_last_index!(idx::Vector, x) = (idx[end] = x; idx)
-
-function assign!(A::AbstractArray, B::AbstractArray{T,N}; last_dim=1:size(B,N)) where {T,N}
-    A_dims, B_dims = size(A), size(B)
-    @assert A_dims[1:end-1] == B_dims[1:end-1] "Inconsistent dimensions with $(A_dims[1:end-1]) and $(B_dims[1:end-1])"
-    A_dims = replace_last_index!(range_indecies(A_dims), last_dim)
-    B_dims = range_indecies(B_dims)
-    A_idxs = CartesianIndices(Tuple(A_dims))
-    B_idxs = CartesianIndices(Tuple(B_dims))
-    for (Aidx, Bidx) = zip(A_idxs, B_idxs)
-        A[Aidx] = B[Bidx]
-    end
-    A
-end
-
-
-
 ## Top-k pooling
 
 function topk_index(y::AbstractVector, k::Integer)
