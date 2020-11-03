b091973701d8a62b12519fa79093c1f8eb15b2d6
Yueh-Hua Tu <a504082002@gmail.com>
2020-11-03 15:32:14 +0800

    Integration of propagate

--- src/cuda/msgpass.jl
+++ src/cuda/msgpass.jl
@@ -1,44 +1,43 @@
-@inline function update_batch_edge(mp::T, adj, E::Fill{S,2,Axes}, X::CuMatrix) where {T<:MessagePassing,S,Axes}
+@inline function update_batch_edge(mp::T, adj, E::Fill{S,2,Axes}, X::CuMatrix, u) where {T<:MessagePassing,S,Axes}
     E = fill(E.value, E.axes)
-    update_batch_edge(mp, adj, E, X)
+    update_batch_edge(mp, adj, E, X, u)
 end
 
-@inline function update_batch_edge(mp::T, adj, E::CuMatrix, X::Fill{S,2,Axes}) where {T<:MessagePassing,S,Axes}
+@inline function update_batch_edge(mp::T, adj, E::CuMatrix, X::Fill{S,2,Axes}, u) where {T<:MessagePassing,S,Axes}
     X = fill(X.value, X.axes)
-    update_batch_edge(mp, adj, E, X)
+    update_batch_edge(mp, adj, E, X, u)
 end
 
-@inline function update_batch_edge(mp::T, adj, E::AbstractMatrix, X::CuMatrix) where {T<:MessagePassing}
+@inline function update_batch_edge(mp::T, adj, E::AbstractMatrix, X::CuMatrix, u) where {T<:MessagePassing}
     E = convert(typeof(X), E)
-    update_batch_edge(mp, adj, E, X)
+    update_batch_edge(mp, adj, E, X, u)
 end
 
-@inline function update_batch_edge(mp::T, adj, E::CuMatrix, X::AbstractMatrix) where {T<:MessagePassing}
+@inline function update_batch_edge(mp::T, adj, E::CuMatrix, X::AbstractMatrix, u) where {T<:MessagePassing}
     X = convert(typeof(E), X)
-    update_batch_edge(mp, adj, E, X)
+    update_batch_edge(mp, adj, E, X, u)
 end
 
-@inline function update_batch_edge(mp::T, adj, E::CuMatrix, X::CuMatrix) where {T<:MessagePassing}
+@inline function update_batch_edge(mp::T, adj, E::CuMatrix, X::CuMatrix, u) where {T<:MessagePassing}
     n = size(adj, 1)
     edge_idx = edge_index_table(adj)
-    hcat([apply_batch_message(mp, i, adj[i], edge_idx, E, X) for i in 1:n]...)
+    hcat([apply_batch_message(mp, i, adj[i], edge_idx, E, X, u) for i in 1:n]...)
 end
 
-@inline function apply_batch_message(mp::T, i, js, edge_idx, E::CuMatrix, X::CuMatrix) where {T<:MessagePassing}
+@inline function apply_batch_message(mp::T, i, js, edge_idx, E::CuMatrix, X::CuMatrix, u) where {T<:MessagePassing}
     hcat([message(mp, get_feature(X, i), get_feature(X, j), get_feature(E, edge_idx[(i,j)])) for j = js]...)
 end
 
-@inline function update_batch_vertex(mp::T, M::AbstractMatrix, X::CuMatrix) where {T<:MessagePassing}
+@inline function update_batch_vertex(mp::T, M::AbstractMatrix, X::CuMatrix, u) where {T<:MessagePassing}
     M = convert(typeof(X), M)
-    update_batch_vertex(mp, M, X)
+    update_batch_vertex(mp, M, X, u)
 end
 
-@inline function update_batch_vertex(mp::T, M::CuMatrix, X::AbstractMatrix) where {T<:MessagePassing}
+@inline function update_batch_vertex(mp::T, M::CuMatrix, X::AbstractMatrix, u) where {T<:MessagePassing}
     X = convert(typeof(M), X)
-    update_batch_vertex(mp, M, X)
+    update_batch_vertex(mp, M, X, u)
 end
 
-@inline function update_batch_vertex(mp::T, M::CuMatrix, X::CuMatrix) where {T<:MessagePassing}
-    X_ = [update(mp, get_feature(M, i), get_feature(X, i)) for i = 1:size(X,2)]
-    hcat(X_...)
+@inline function update_batch_vertex(mp::T, M::CuMatrix, X::CuMatrix, u) where {T<:MessagePassing}
+    hcat([update(mp, get_feature(M, i), get_feature(X, i)) for i = 1:size(X,2)]...)
 end
--- src/layers/msgpass.jl
+++ src/layers/msgpass.jl
@@ -29,17 +29,17 @@ First argument should be message-passing layer, the rest of arguments can be `X`
 @inline update(mp::T, m, x) where {T<:MessagePassing} = m
 @inline update(mp::T, i::Integer, m, x) where {T<:MessagePassing} = m
 
-@inline function update_batch_edge(mp::T, adj, E::AbstractMatrix, X::AbstractMatrix) where {T<:MessagePassing}
+@inline function update_batch_edge(mp::T, adj, E::AbstractMatrix, X::AbstractMatrix, u) where {T<:MessagePassing}
     n = size(adj, 1)
     edge_idx = edge_index_table(adj)
-    hcat([apply_batch_message(mp, i, adj[i], edge_idx, E, X) for i in 1:n]...)
+    hcat([apply_batch_message(mp, i, adj[i], edge_idx, E, X, u) for i in 1:n]...)
 end
 
-@inline function apply_batch_message(mp::T, i, js, edge_idx, E::AbstractMatrix, X::AbstractMatrix) where {T<:MessagePassing}
+@inline function apply_batch_message(mp::T, i, js, edge_idx, E::AbstractMatrix, X::AbstractMatrix, u) where {T<:MessagePassing}
     hcat([message(mp, get_feature(X, i), get_feature(X, j), get_feature(E, edge_idx[(i,j)])) for j = js]...)
 end
 
-@inline function update_batch_vertex(mp::T, M::AbstractMatrix, X::AbstractMatrix) where {T<:MessagePassing}
+@inline function update_batch_vertex(mp::T, M::AbstractMatrix, X::AbstractMatrix, u) where {T<:MessagePassing}
     hcat([update(mp, get_feature(M, i), get_feature(X, i)) for i in 1:size(X,2)]...)
 end
 
@@ -51,15 +51,10 @@ end
 
 function propagate(mp::T, fg::FeaturedGraph, aggr::Symbol=:add) where {T<:MessagePassing}
     E, X = propagate(mp, adjacency_list(fg), fg.ef, fg.nf, aggr)
-    FeaturedGraph(graph(fg), X, E, zeros(0))
+    FeaturedGraph(graph(fg), X, E, Fill(0.f0, 0))
 end
 
 function propagate(mp::T, adj::AbstractVector{S}, E::R, X::Q, aggr::Symbol) where {T<:MessagePassing,S<:AbstractVector,R,Q}
-    accu_edge = accumulated_edges(adj)
-
-    E = update_batch_edge(mp, adj, E, X)
-    M = aggregate_neighbors(mp, aggr, E, accu_edge)
-    X = update_batch_vertex(mp, M, X)
-
+    E, X, u = propagate(mp, adj, E, X, Fill(0.f0, 0), aggr, nothing, nothing)
     E, X
 end
