995b4d8c3d4bb3a786b4f77dc2a3c4b4a126f9a4
Yueh-Hua Tu <a504082002@gmail.com>
2022-01-08 22:11:55 +0800

    Replace bypass_graph as Bypass layer

--- test/layers/misc.jl
+++ test/layers/misc.jl
@@ -1,24 +1,26 @@
 @testset "misc" begin
-    @testset "bypass_graph" begin
+    @testset "Bypass" begin
+        T = Float32
         N = 4
         E = 5
-        adj = [0 1 1 1;
-            1 0 1 0;
-            1 1 0 1;
-            1 0 1 0]
+        adj = T[0 1 1 1;
+                1 0 1 0;
+                1 1 0 1;
+                1 0 1 0]
 
         nf = rand(3, N)
         ef = rand(5, E)
         gf = rand(7)
+        nodes = [1,2,4]
 
         fg = FeaturedGraph(adj, nf=nf, ef=ef, gf=gf)
-        layer = bypass_graph(x -> x .+ 1.,
-                                x -> x .+ 2.,
-                                x -> x .+ 3.)
-        fg_ = layer(fg)
-        @test GraphSignals.adjacency_matrix(fg_) == adj
-        @test node_feature(fg_) == nf .+ 1.
-        @test edge_feature(fg_) == ef .+ 2.
-        @test global_feature(fg_) == gf .+ 3.
+        fsg = subgraph(fg, nodes)
+
+        layer = Bypass(node_layer=Dropout(0.5),
+                       global_layer=x -> x .+ 3.)
+        fsg_ = layer(fsg)
+        @test node_feature(fsg_) == view(nf, :, nodes)
+        @test edge_feature(fsg_) == view(ef, :, edges(fsg))
+        @test global_feature(fsg_) == gf .+ 3.
     end
 end
