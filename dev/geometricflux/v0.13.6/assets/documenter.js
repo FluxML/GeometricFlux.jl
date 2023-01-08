89ac12ca0080b2f24e5f5907a2f235ab6bdbae8e
Documenter.jl <documenter@juliadocs.github.io>
2023-01-08 22:58:49 +0000

    build based on 63265df

--- dev/geometricflux/v0.13.6/assets/documenter.js
+++ dev/geometricflux/v0.13.6/assets/documenter.js
@@ -329,23 +329,3 @@ $(document).ready(function() {
 })
 
 })
-// injected into documenter.js by MultiDocumenter.jl
-// require(["jquery", "headroom", "headroom-jquery"], function ($, Headroom) {
-//   $(document).ready(function () {
-//     $("#multi-page-nav").headroom({
-//       tolerance: { up: 10, down: 10 },
-//     });
-//   });
-// });
-require(["jquery"], function ($) {
-  $(document).ready(function () {
-    document
-      .getElementById("multidoc-toggler")
-      .addEventListener("click", function () {
-        document
-          .getElementById("nav-items")
-          .classList.toggle("hidden-on-mobile");
-      });
-  });
-});
-
